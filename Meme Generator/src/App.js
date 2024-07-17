944f322ab5424a0474b932dddbff7e9f81c877b8
dharm-upenn <146027574+dharm-upenn@users.noreply.github.com>
2024-07-17 19:44:17 -0400

    Add files via upload

---
+++ Meme Generator/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Header from "./Components/Header"
+import Meme from "./Components/Meme"
+
+export default function App(){
+  return(
+    <>
+      <Header/>
+      <Meme/>
+    </>
+  )
+}
